da428eef49c77932aa08634ab86afc0b7aec788a
Hector Saul Maldonado Gomez <saul_99_27@hotmail.com>
2020-09-10 21:57:53 -0500

    Proyecto finalizado

---
+++ ProgramacionWebEjerciciosU2/Ejercicio1_Suma/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ejercicio1_Suma.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio1_Suma.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ResultadoSuma(Numeros n)
+        {
+            int resultado = n.numero1 + n.numero2;
+            return View(resultado);
+        }
+    }
+}
---
+++ ProgramacionWebEjerciciosU2/Ejercicio1_Suma/Models/Numeros.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_Suma.Models
+{
+    public class Numeros
+    {
+        public int numero1 { get; set; }
+        public int numero2 { get; set; }
+    }
+}
