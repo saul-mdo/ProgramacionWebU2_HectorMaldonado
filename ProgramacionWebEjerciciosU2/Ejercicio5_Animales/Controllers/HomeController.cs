f703b6c90146ca59e898fbd1a14f627c0f19cd35
Hector Saul Maldonado Gomez <saul_99_27@hotmail.com>
2020-10-01 09:16:15 -0500

    Avance actividad 5

--- ProgramacionWebEjerciciosU2/Ejercicio5_Animales/Controllers/HomeController.cs
+++ ProgramacionWebEjerciciosU2/Ejercicio5_Animales/Controllers/HomeController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ejercicio5_Animales.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ejercicio5_Animales.Controllers
 {
@@ -10,7 +12,22 @@ namespace Ejercicio5_Animales.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            animalesContext context = new animalesContext();
+
+            var clases = context.Clase.OrderBy(x => x.Nombre);
+
+            return View(clases);
+        }
+        [Route("/Especie/{id}")]
+        public IActionResult Especies(string id)
+        {
+            animalesContext context = new animalesContext();
+
+            var nombre = id.Replace("-", " ").ToUpper();
+
+            var especies = context.Clase.Include(x => x.Especies).Where(x => x.Nombre.ToUpper() == nombre).OrderBy(x => x.Nombre);
+
+            return View(especies);
         }
     }
 }
